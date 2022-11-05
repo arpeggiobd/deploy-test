60b86b41bfa9b8d6ef6de3074bb9783b714802b6
Chester Wong <wongjl.chester@gmail.com>
2022-11-05 11:28:39 +0800

    9th commit

--- apps/server/server.js
+++ apps/server/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require('path')
 
 app.use(express.static("../client/dist"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
+});
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve("../client/dist/index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
